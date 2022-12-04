bd19e08bc69a426af63dfa10e48e03e4b9fe519c
Gabriel Assanuma <gabriel.assanuma@gmail.com>
2022-12-04 15:35:19 +0000

    edit form done

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,13 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
+
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:phone_number, :username, :street_name, :house_number, :additional_info, :email, :password) }
+
+    devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:phone_number, :username, :street_name, :house_number, :additional_info, :email, :password, :current_password) }
+  end
 end

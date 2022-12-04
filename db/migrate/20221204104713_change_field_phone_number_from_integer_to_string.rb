562658bacaabe8889d8284233ecd13651eb39ecf
Gabriel Assanuma <gabriel.assanuma@gmail.com>
2022-12-04 10:49:17 +0000

    changed column phone_number to string

---
+++ db/migrate/20221204104713_change_field_phone_number_from_integer_to_string.rb
@@ -0,0 +1,5 @@
+class ChangeFieldPhoneNumberFromIntegerToString < ActiveRecord::Migration[7.0]
+  def change
+    change_column :users, :phone_number, :string
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_12_04_101434) do
+ActiveRecord::Schema[7.0].define(version: 2022_12_04_104713) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -49,7 +49,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_12_04_101434) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "username"
-    t.integer "phone_number"
+    t.string "phone_number"
     t.string "street_name"
     t.integer "house_number"
     t.string "additional_info"

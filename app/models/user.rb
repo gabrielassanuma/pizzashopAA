04d218f1ebc2b93c0624224a8d1bca2af4925b17
Gabriel Assanuma <gabriel.assanuma@gmail.com>
2022-12-06 09:16:07 +0000

    added validations on user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,9 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_many :order
-  validates :phone_number, uniqueness: true
+  validates :phone_number, length: { is: 9 }, uniqueness: true
+  validates :password, length: { in: 6..20 }
+  validates :phone_number, :username, :street_name, :house_number, presence: true
 
   def email_required?
     false

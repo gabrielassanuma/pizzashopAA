c7eb9d4276cde45ac967832b96a53455eaf710e6
gabriel assanuma <gabriel.assanuma@gmail.com>
2022-12-31 10:03:59 +0000

    refactoring order_products controller

--- app/controllers/order_products_controller.rb
+++ app/controllers/order_products_controller.rb
@@ -1,28 +1,26 @@
 class OrderProductsController < ApplicationController
+  before_action :set_order_product, only: [:add_quantity, :reduce_quantity, :destroy]
+
   def create
     chosen_product = Product.find(params[:product_id])
     current_cart = @current_cart
-    if current_cart.products.include?(chosen_product)
-      @order_product = current_cart.order_products.find_by(:product_id => chosen_product)
-      @order_product.quantity += 1
-    else
-      @order_product = OrderProduct.new
+    @order_product = current_cart.order_products.find_or_initialize_by(product: chosen_product)
+    if @order_product.new_record?
       @order_product.cart = current_cart
-      @order_product.product = chosen_product
+    else
+      @order_product.quantity += 1
     end
     @order_product.save
     redirect_to cart_path(@current_cart)
   end 
 
   def add_quantity
-    @order_product = OrderProduct.find(params[:id])
     @order_product.quantity += 1
     @order_product.save
     redirect_to cart_path(@current_cart)
   end
   
   def reduce_quantity
-    @order_product = OrderProduct.find(params[:id])
     if @order_product.quantity > 1
       @order_product.quantity -= 1
     end
@@ -31,14 +29,21 @@ class OrderProductsController < ApplicationController
   end
 
   def destroy
-    @order_product = OrderProduct.find(params[:id])
     @order_product.destroy
     redirect_to cart_path(@current_cart)
   end 
     
   private
   
+  def set_order_product
+    @order_product = OrderProduct.find(params[:id])
+  end
+  
   def order_product_params
     params.require(:order_product).permit(:quantity, :product_id, :cart_id)
   end
 end
+
+
+
+

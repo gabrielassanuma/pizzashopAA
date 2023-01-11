c3f22c423dd08921c61576dd81d4b40f019c4230
gabriel assanuma <gabriel.assanuma@gmail.com>
2023-01-11 11:08:31 +0000

    delete railsway set up

--- config/application.rb
+++ config/application.rb
@@ -6,7 +6,7 @@ require "rails/all"
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-config.hosts << /[a-z0-9\-]+\.up\.railway\.app/
+
 
 module PizzashopAA
   class Application < Rails::Application

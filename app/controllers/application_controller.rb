c7dfd06163521e736ddffb5b97b4d25f6576fca5
gabriel assanuma <gabriel.assanuma@gmail.com>
2023-01-12 15:39:36 +0000

    debug address

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -24,9 +24,9 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:phone_number, :username, :street_name, :house_number, :additional_info, :email, :password) }
+    devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:phone_number, :username, :address, :additional_info, :email, :password) }
 
-    devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:phone_number, :username, :street_name, :house_number, :additional_info, :email, :password, :current_password) }
+    devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:phone_number, :username, :address, :additional_info, :email, :password, :current_password) }
   end
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,11 +4,11 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_many :order
+  geocoded_by :address
+  after_validation :geocode, if: :will_save_change_to_address?
   validates :phone_number, length: { is: 9 }, uniqueness: true
   validates :password, length: { in: 6..20 }
   validates :phone_number, :username, :address, presence: true
-  geocoded_by :address
-  after_validation :geocode, if: :will_save_change_to_address?
 
   def email_required?
     false

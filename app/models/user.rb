27d13409715660f61d22b4711b4ad117f150938c
gabriel assanuma <gabriel.assanuma@gmail.com>
2023-01-13 09:55:26 +0000

    add validation on user model by distance from store

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,11 +4,12 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_many :order
-  geocoded_by :address
-  after_validation :geocode, if: :will_save_change_to_address?
   validates :phone_number, length: { is: 9 }, uniqueness: true
   validates :password, length: { in: 6..20 }
   validates :phone_number, :username, :address, presence: true
+  geocoded_by :address
+  validate :distance_from_specific_point
+  after_validation :geocode, if: :will_save_change_to_address?
 
   def email_required?
     false
@@ -18,4 +19,12 @@ class User < ApplicationRecord
     false
   end
 
+  def distance_from_specific_point
+    if self.latitude.present? && self.longitude.present?
+      distance = Geocoder::Calculations.distance_between([self.latitude, self.longitude], [38.7246365, -9.1509999])
+      if distance > 5
+        errors.add(:address, "is too far from the specific point")
+      end
+    end
+  end
 end

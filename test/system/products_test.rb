4fc57e58886a354d63af9877b774c543fe439f80
gabriel assanuma <gabriel.assanuma@gmail.com>
2023-01-17 18:04:43 +0000

    set up application test with test-min and capybara

---
+++ test/system/products_test.rb
@@ -0,0 +1,8 @@
+require "application_system_test_case"
+
+class ProductsTest < ApplicationSystemTestCase
+  test "Products index" do
+    visit products_url
+    assert_selector "h1", text: "Products"
+  end
+end

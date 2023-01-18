0bda872570fb8740d3f70518cb7055aa78464519
gabriel assanuma <gabriel.assanuma@gmail.com>
2023-01-18 14:56:13 +0000

    changed models to fix delivery-fee bug

--- app/models/order.rb
+++ app/models/order.rb
@@ -2,7 +2,7 @@ class Order < ApplicationRecord
   belongs_to :user
   has_many :order_products
   has_one :ddriver
-  has_one :delivery_fee, through: :user
+  has_one :delivery_fee
 
 
   def ready!
--- db/migrate/20230115102243_add_delivery_fees_id_to_users.rb
+++
@@ -1,5 +0,0 @@
-class AddDeliveryFeesIdToUsers < ActiveRecord::Migration[7.0]
-  def change
-    add_reference :users, :delivery_fee, foreign_key: true
-  end
-end

6d2a4c584247edf76c8c991e94918a0b162f2df3
gabriel assanuma <gabriel.assanuma@gmail.com>
2023-01-18 18:08:35 +0000

    added product model rspec test

--- spec/models/order_spec.rb
+++ spec/models/order_spec.rb
@@ -2,15 +2,13 @@ require 'rails_helper'
 
 RSpec.describe Order, type: :model do
   let(:user) {User.create!(phone_number: 999888777, password: 111111, username: "Gabriel", address: "Calçada da Pampulha 27, Lisboa", latitude: 38.7046306, longitude: -9.167364) }
-  let(:ddriver) { Ddriver.create!(first_name: "Elon", last_name: "Musk", active: true) }
-  let(:delivery_fee) { DeliveryFee.create!(zone: "A", price: 2) }
-  let(:order) { Order.create!(user: user, total_price: 100, order_status: 'new_order', ddriver: ddriver, take_away_name: '', take_away_phone: '', delivery_fee: "delivery_fee" ) }
+  let(:order) { Order.create!(user: user, total_price: 100, order_status: 'new_order' ) }
 
 
   describe '#ready!' do 
     it "should mark order as ready" do
       order.ready!
-      expect(order.order_status).to eq('')
+      expect(order.order_status).to eq('ready')
     end
   end
 end
---
+++ spec/models/product_spec.rb
@@ -0,0 +1,11 @@
+require 'rails_helper'
+
+RSpec.describe Product, type: :model do
+  describe "#deactive!" do
+    it "should deactive product" do
+      product = Product.create!(name: "product", description: "description", price: 10, subclass: "test", active: true)
+      product.deactive!
+      expect(product.active).to eq(false)
+    end
+  end
+end

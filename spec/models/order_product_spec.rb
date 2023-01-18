7a1a68bc046fc1a0169d3ade8b6232513a47b36f
gabriel assanuma <gabriel.assanuma@gmail.com>
2023-01-18 17:37:06 +0000

    added order product and order rspec

--- spec/models/order_product_spec.rb
+++ spec/models/order_product_spec.rb
@@ -1,16 +1,15 @@
-# require 'rails_helper'
+require 'rails_helper'
 
-# RSpec.describe OrderProduct, type: :model do
-#   let(:delivery_fee) {DeliveryFee.create!(zone: "A", price: 2) }
-#   let(:user) {User.create!(phone_number: 999888777, password: 111111, username: "Gabriel", address: "Calçada da Pampulha 27, Lisboa", latitude: 38.7046306, longitude: -9.167364, delivery_fee: delivery_fee) }
-#   let(:order) { Order.create!(user: user)}
-#   let(:product) { Product.create!(name: "Delicious", description: "Super yumi", price: 10, subclass:"test", active: true) }
-#   let(:cart) { Cart.create! }
-#   let(:order_product) { OrderProduct.create!(order: order, product: product, quantity: 2, cart: cart) }
+RSpec.describe OrderProduct, type: :model do
+  let(:delivery_fee) {DeliveryFee.create!(zone: "A", price: 2) }
+  let(:user) {User.create!(phone_number: 999888777, password: 111111, username: "Gabriel", address: "Calçada da Pampulha 27, Lisboa", latitude: 38.7046306, longitude: -9.167364) }
+  let(:product) { Product.create!(name: "Delicious", description: "Super yumi", price: 10, subclass:"test", active: true) }
+  let(:cart) { Cart.create! }
+  let(:order_product) { OrderProduct.create!(product: product, quantity: 2, cart: cart) }
 
-#   describe "#total_price" do
-#     it "returns total price for the order product" do
-#       expect(order_product.total_price).to eq(20)
-#     end
-#   end
-# end
+  describe "#total_price" do
+    it "returns total price for the order product" do
+      expect(order_product.total_price).to eq(20)
+    end
+  end
+end
--- spec/models/order_spec.rb
+++ spec/models/order_spec.rb
@@ -1,9 +1,16 @@
-# require 'rails_helper'
+require 'rails_helper'
 
-# RSpec.describe Order, type: :model do
-#   describe '#ready!' do 
-#   it "should mark order as ready"
-#     order = Order.create!(user_id: 1, total_price: 100, order_status: 'new_order', ddriver_id: 1, take_away_name: '', take_away_phone: '')
-#     expect(order.ready!).to eq('')
-#   end
-# end
+RSpec.describe Order, type: :model do
+  let(:user) {User.create!(phone_number: 999888777, password: 111111, username: "Gabriel", address: "Calçada da Pampulha 27, Lisboa", latitude: 38.7046306, longitude: -9.167364) }
+  let(:ddriver) { Ddriver.create!(first_name: "Elon", last_name: "Musk", active: true) }
+  let(:delivery_fee) { DeliveryFee.create!(zone: "A", price: 2) }
+  let(:order) { Order.create!(user: user, total_price: 100, order_status: 'new_order', ddriver: ddriver, take_away_name: '', take_away_phone: '', delivery_fee: "delivery_fee" ) }
+
+
+  describe '#ready!' do 
+    it "should mark order as ready" do
+      order.ready!
+      expect(order.order_status).to eq('')
+    end
+  end
+end

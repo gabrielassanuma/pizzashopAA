bb8583d06ea467694a36dc70dc841255b1b08177
gabriel assanuma <gabriel.assanuma@gmail.com>
2023-01-21 16:02:10 +0000

    added crud test on ddriver

--- spec/controllers/ddrivers_spec.rb
+++ spec/controllers/ddrivers_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe DdriversController, type: :controller do 
-  describe "#index"do
+  describe "#index" do
     it "assign all ddrivers to @ddrivers" do
       sign_in(create(:user))
       ddriver = create(:ddriver) 
@@ -9,4 +9,22 @@ RSpec.describe DdriversController, type: :controller do
       expect(assigns(:ddrivers)).to eq([ddriver])
     end 
   end
+
+  describe "#show" do
+    it "assigns ddriver to @ddriver" do
+      sign_in(create(:user))
+      ddriver = create(:ddriver)
+      get :show, params: {id: ddriver.id}
+      expect(assigns(:ddriver)).to eq(ddriver)
+    end
+  end 
+
+  describe '#new' do
+    it 'assigns a new ddriver to @ddriver' do
+      sign_in(create(:user))
+      get :new
+      expect(assigns(:ddriver)).to be_a_new(Ddriver)
+    end
+  end
+
 end

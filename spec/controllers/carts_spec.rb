01e09b3ebf8529acf28336ff6eb761a1e4cda6f1
gabriel assanuma <gabriel.assanuma@gmail.com>
2023-01-25 15:41:18 +0000

    Rspec tests and factory changes

--- spec/controllers/carts_spec.rb
+++ spec/controllers/carts_spec.rb
@@ -1,10 +1,12 @@
-# require 'rails_helper'
+require 'rails_helper'
 
-# RSpec.describe CartsController, type: :controller do
-#   describe "GET#show" do
-#     it "assigns the current cart to @cart" do
-
-#       expect(assigns(:cart)).to eq(@current_cart)
-#     end
-#   end
-# end
+RSpec.describe CartsController, type: :controller do
+  describe 'GET#show' do
+    it 'assigns the current cart to @cart' do
+      cart = create(:cart)
+      session[:cart_id] = cart.id
+      get :show, params: {id: cart.id }
+      expect(assigns(:cart)).to eq cart
+    end
+  end
+end 
--- spec/factories/cart.rb
+++ spec/factories/cart.rb
@@ -1,6 +1,5 @@
 FactoryBot.define do
   factory :cart do
-    id { 1 }
     trait :invalid do
     end
   end

3ba4e5e2811542042dfb8dc3006044f53054fa68
gabriel assanuma <gabriel.assanuma@gmail.com>
2023-01-27 11:09:01 +0000

    changes on RSpec model cart

--- spec/models/cart_spec.rb
+++ spec/models/cart_spec.rb
@@ -2,8 +2,8 @@ require 'rails_helper'
 
 RSpec.describe Cart, type: :model do
   let(:cart) { Cart.create! }
-  let(:product1) { Product.create!(name: "Product 1", price: 10.00) }
-  let(:product2) { Product.create!(name: "Product 2", price: 20.00) }
+  let(:product1) { Product.create!(name: "Product 1", description: "cool description", price: 10.00, subclass: "pizza", active: true) }
+  let(:product2) { Product.create!(name: "Product 2", description: "cool description", price: 10.00, subclass: "pizza", active: true) }
   let(:order_product1) { OrderProduct.create!(product: product1, cart: cart, quantity: 2) }
   let(:order_product2) { OrderProduct.create!(product: product2, cart: cart, quantity: 3) }
 
@@ -14,7 +14,7 @@ RSpec.describe Cart, type: :model do
 
   describe "#sub_total" do
     it "returns the correct subtotal for the cart" do
-      expect(cart.sub_total).to eq(80.00)
+      expect(cart.sub_total).to eq(50.00)
     end
   end
 

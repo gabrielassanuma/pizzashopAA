522a7462f0ec777e0c190b2120d57bedb70c87b6
gabriel assanuma <gabriel.assanuma@gmail.com>
2023-01-29 14:48:04 +0000

    added Rspec tests for application controller

--- spec/controllers/application_spec.rb
+++ spec/controllers/application_spec.rb
@@ -1,12 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe ApplicationController, type: :controller do
-  describe "#set_current_cart" do
-    it "assigns @current_cart" do
-      sign_in(create(:user))
+  describe '#set_current_cart' do
+    it 'sets the current cart based on the session' do
       cart = create(:cart)
       session[:cart_id] = cart.id
+      allow(Cart).to receive(:find_by).and_return(cart)
+      controller.send(:set_current_cart)
       expect(assigns(:current_cart)).to eq(cart)
+    end
+
+    it 'creates a new cart if one is not found in the session' do
+      cart = nil
+      session[:cart_id] = nil
+      allow(Cart).to receive(:find_by).and_return(cart)
+      controller.send(:set_current_cart)
+      expect(assigns(:current_cart)).not_to be_nil
     end
   end
 end

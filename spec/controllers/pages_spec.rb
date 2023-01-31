0f91935830e0f1e45056244cb4a1daaf5219957c
gabriel assanuma <gabriel.assanuma@gmail.com>
2023-01-31 16:41:20 +0000

    added more tests on pages controller RSpec

--- spec/controllers/pages_spec.rb
+++ spec/controllers/pages_spec.rb
@@ -4,9 +4,47 @@ RSpec.describe PagesController, type: :controller do
   describe "GET#home" do
     it "should set user to current_user" do
       user = create(:user)
-      sign_in(user)
+      sign_in user
       get :home
-      
+      expect(assigns(:user)).to eq(user)
+    end
+  end
+
+  describe "GET#myorders" do
+    it "should set all order from user to @orders" do
+      user = create(:user)
+      sign_in user
+      order = create(:order, user_id: user.id)
+      get :my_orders
+      expect(assigns(:orders)).to eq([order])
+    end
+  end
+
+  describe "GET#pizzas" do
+    it "should set all products subclass: 'pizza' to @pizzas" do
+      user = create(:user, :admin)
+      sign_in user
+      pizza = create(:product, subclass: "pizza")
+      get :pizzas
+      expect(assigns(:pizzas)).to eq([pizza])
+    end
+  end
+
+  describe "GET#drinks" do
+    it "should set all products subclass: 'drink' to @drinks" do
+      sign_in(create(:user, :admin))
+      drink = create(:product, subclass: "drink")
+      get :drinks
+      expect(assigns(:drinks)).to eq([drink])
+    end
+  end
+
+  describe "GET#desserts" do
+    it "should set all products subclass: 'dessert' to @desserts" do
+      sign_in(create(:user, :admin))
+      dessert = create(:product, subclass: "dessert")
+      get :desserts
+      expect(assigns(:desserts)).to eq([dessert])
     end
   end
 end
